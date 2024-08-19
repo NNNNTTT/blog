11d48cdd4c7f59769c1321bc14c42dc512f9d7ef
坪田直樹 <tsubotanaoki@tsubotanaokinoMacBook-Pro.local>
2024-08-19 21:29:17 +0900

    20240819

--- config/settings.py
+++ config/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog_db',
+        'USER': 'tsubota-naoki',
+        'PASSWORD': 'tsubota-naoki',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
